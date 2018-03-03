4262e362a19429ec18902485b1dfc28f5b5e25d9
DBattou <doucerain.b@gmail.com>
2018-03-03 19:19:52 +0100

    refacto bowling

--- bowling.rb
+++ bowling.rb
@@ -1,41 +1,57 @@
 class Bowling
   attr_accessor :score
 
-  def initialize(rolls)
-    @index = 0
+  def initialize
     @score = 0
-    rolls.each_char do
-      calculateScore(rolls)
+    @gameRolls = []
+  end
+
+  def hit(pins_count)
+    @gameRolls.push(pins_count)
+  end
+
+  def giveMeTheScore
+    (0..9).each do |indexFrame|
+      if isSpare?(indexFrame)
+        addSpareBonusToScore(indexFrame)
+      elsif isStrike?(indexFrame)
+        addStrikeBonusToScore(indexFrame)
+      end
+      addFrameScore(indexFrame)
+      p @score
+    end
+    return @score
+  end
+
+  private
+  def addStrikeBonusToScore(indexFrame)
+    nextFrame = indexFrame + 1
+    secondNextFrame = indexFrame + 2
+    if isStrike?(nextFrame) and isStrike?(secondNextFrame)
+      @score += 20
+    elsif isStrike?(nextFrame)
+      @score += 10 + @gameRolls[2 * secondNextFrame]
+    else
+      @score += @gameRolls[2 * nextFrame] + @gameRolls[2 * nextFrame + 1]
     end
   end
 
-  def calculateScore(rolls)
-    char = rolls[@index]
-    prevChar = rolls[@index - 1]
-    nextChar = rolls[@index + 1]
-    nextChar2 = rolls[@index + 2]
-    @index += 1
-    if char == 'X'
-      @score += 10
-      if nextChar2 == '/'
-        @score += 10
-      elsif nextChar == 'X' && @index < 10
-        @score += 10
-        if nextChar2 == 'X' && @index < 10
-          @score += 10
-        else
-          @score += nextChar2.to_i
-        end
-      else
-        @score += nextChar.to_i + nextChar2.to_i
-      end
-    elsif char == '/'
-      if nextChar == 'X'
-        @score += 10
-      end
-      @score += 10 - prevChar.to_i + nextChar.to_i
-    else
-      @score += char.to_i
+  def addFrameScore(indexFrame)
+    @score += @gameRolls[2 * indexFrame] + @gameRolls[2 * indexFrame + 1]
+  end
+
+  def addSpareBonusToScore(indexFrame)
+    @score += @gameRolls[2 * (indexFrame + 1)]
+  end
+
+  def isSpare?(indexFrame)
+    if @gameRolls[2 * indexFrame] == 10
+      return false
     end
+    return (@gameRolls[2 * indexFrame] + @gameRolls[2 * indexFrame + 1]) == 10
+  end
+
+  def isStrike?(indexFrame)
+    return @gameRolls[2 * indexFrame] == 10
   end
 end
--- bowling_spec.rb
+++ bowling_spec.rb
@@ -1,80 +1,89 @@
 require 'rspec'
-require_relative 'bowling.rb'
+require_relative 'bowling'
 
-describe 'Bowling score' do
-
-  it 'should be null' do
-    game = Bowling.new("0"*20)
-    expect(game.score).to be(0)
+RSpec.describe Bowling, "#score" do
+  context "Perfect gutter game no points at all" do
+    it "Sums the pin count for each rolls" do
+      bowling = Bowling.new
+      20.times {bowling.hit(0)}
+      expect(bowling.giveMeTheScore).to be(0)
+    end
   end
 
-  it 'one roll with 5 pins' do
-    game = Bowling.new("5" + "0" * 19)
-    expect(game.score).to be(5)
+  context "Game with some points but no spare or strike" do
+    it "Sums the pin count for each rolls" do
+      bowling = Bowling.new
+      20.times {bowling.hit(2)}
+      expect(bowling.giveMeTheScore).to be(40)
+    end
   end
 
-  it '5 good rolls without strikes or sper' do
-    game = Bowling.new("546786" + "0" * 15)
-    expect(game.score).to be(36)
+  context "Game with ONE spare at the beginning and only 2pts hits right after" do
+    it "Sums the pin count for each rolls" do
+      bowling = Bowling.new
+      bowling.hit(2)
+      bowling.hit(8)
+      18.times{bowling.hit(2)}
+      expect(bowling.giveMeTheScore).to be(48)
+    end
   end
 
-  it 'one sper with 2 pins at the first roll' do
-    game = Bowling.new("2/" + "0" * 15)
-    expect(game.score).to be(10)
+  context "Game with TWO spare in a row and only 2pts hits right after" do
+    it "Sums the pin count for each rolls" do
+      bowling = Bowling.new
+      bowling.hit(2)
+      bowling.hit(8)
+      bowling.hit(4)
+      bowling.hit(6)
+      16.times{bowling.hit(2)}
+      expect(bowling.giveMeTheScore).to be(58)
+    end
   end
 
-  it 'one sper with 2 pins at the first roll and 2 at the next roll' do
-    game = Bowling.new("2/2" + "0" * 16)
-    expect(game.score).to be(14)
+  context "Game with ONLY spare" do
+    it "Sums the pin count for each rolls" do
+      bowling = Bowling.new
+      10.times{
+        bowling.hit(2)
+        bowling.hit(8)
+      }
+      bowling.hit(6)
+      expect(bowling.giveMeTheScore).to be(124)
+    end
   end
 
-  it '2sper in a row' do
-    game = Bowling.new("2/2/" + "0" * 16)
-    expect(game.score).to be(22)
+  context "Game with one strike and only 2pts hits right after" do
+    it "Sums the pin count for each rolls" do
+      bowling = Bowling.new
+      bowling.hit(10)
+      bowling.hit(0)
+      18.times{bowling.hit(2)}
+      expect(bowling.giveMeTheScore).to be(50)
+    end
   end
 
-  it '1strike followed by non sper frames' do
-    game = Bowling.new("X2451" + "0" * 14)
-    expect(game.score).to be(28)
+  context "Game with 2 strike in a row and only 2pts hit right after" do
+    it "Sums the pin count for each rolls" do
+      bowling = Bowling.new
+      2.times{
+        bowling.hit(10)
+        bowling.hit(0)
+      }
+      16.times{bowling.hit(2)}
+      expect(bowling.giveMeTheScore).to be(68)
+    end
   end
 
-  it '1strike followed by sper' do
-    game = Bowling.new("X2/" + "0" * 14)
-    expect(game.score).to be(30)
-  end
-
-  it 'sper followed by strike' do
-    game = Bowling.new("2/X" + "0" * 14)
-    expect(game.score).to be(30)
-  end
-
-  it '2 strike in a row' do
-    game = Bowling.new("XX" + "0" * 14)
-    expect(game.score).to be(30)
-  end
-
-  it '3 strike in a row' do
-    game = Bowling.new("XXX" + "0" * 14)
-    expect(game.score).to be(60)
-  end
-
-  it '7 strike in a row' do
-    game = Bowling.new("XXXXXXX" + "0" * 14)
-    expect(game.score).to be(180)
-  end
-
-  it '3 sper in a row' do
-    game = Bowling.new("5/4/6/" + "0" * 14)
-    expect(game.score).to be(40)
-  end
-
-  it '7 sper/strikes in a row' do
-    game = Bowling.new("5/X5/X5/X5/X" + "0" * 4)
-    expect(game.score).to be(150)
-  end
-
-  it 'only strikes' do
-    game = Bowling.new("XXXXXXXXXXXX")
-    expect(game.score).to be(300)
+  context "Game only strike" do
+    it "Sums the pin count for each rolls" do
+      bowling = Bowling.new
+      10.times{
+        bowling.hit(10)
+        bowling.hit(0)
+      }
+      bowling.hit(2)
+      bowling.hit(6)
+      expect(bowling.giveMeTheScore).to be(280)
+    end
   end
 end
